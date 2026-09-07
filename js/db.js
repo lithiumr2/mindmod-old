5cd550d07d7032b23187ffc77ad645b3e29b0266
lithium <lithium091198@gmail.com>
2026-09-07 18:42:31 -0400

    Update db.js

--- js/db.js
+++ js/db.js
@@ -1,91 +1,69 @@
-class Database {
-    constructor() {
-        this.dbName = 'MindustryModStudioDB';
-        this.version = 1;
-        this.db = null;
+const DB = {
+  name: 'MindmodDB',
+  version: 1,
+  db: null,
+
+  async init() {
+    return new Promise((resolve, reject) => {
+      const request = indexedDB.open(this.name, this.version);
+      
+      request.onupgradeneeded = (e) => {
+        const db = e.target.result;
+        if (!db.objectStoreNames.contains('files')) {
+          db.createObjectStore('files', { keyPath: 'name' });
+        }
+      };
+      
+      request.onsuccess = (e) => {
+        this.db = e.target.result;
+        resolve();
+      };
+      
+      request.onerror = () => reject('Error al abrir IndexedDB');
+    });
+  },
+
+  async saveFile(name, content, type = 'hjson') {
+    return new Promise((resolve) => {
+      const tx = this.db.transaction('files', 'readwrite');
+      const store = tx.objectStore('files');
+      store.put({ name, content, type, lastModified: Date.now() });
+      tx.oncomplete = () => resolve();
+    });
+  },
+
+  async getFile(name) {
+    return new Promise((resolve) => {
+      const tx = this.db.transaction('files', 'readonly');
+      const store = tx.objectStore('files');
+      const request = store.get(name);
+      request.onsuccess = () => resolve(request.result);
+    });
+  },
+
+  async getAllFiles() {
+    return new Promise((resolve) => {
+      const tx = this.db.transaction('files', 'readonly');
+      const store = tx.objectStore('files');
+      const request = store.getAll();
+      request.onsuccess = () => resolve(request.result || []);
+    });
+  },
+
+  async deleteFile(name) {
+    return new Promise((resolve) => {
+      const tx = this.db.transaction('files', 'readwrite');
+      const store = tx.objectStore('files');
+      store.delete(name);
+      tx.oncomplete = () => resolve();
+    });
+  },
+
+  async renameFile(oldName, newName) {
+    const file = await this.getFile(oldName);
+    if (file) {
+      await this.saveFile(newName, file.content, file.type);
+      await this.deleteFile(oldName);
     }
-
-    async init() {
-        return new Promise((resolve, reject) => {
-            const request = indexedDB.open(this.dbName, this.version);
-            request.onupgradeneeded = (e) => {
-                const db = e.target.result;
-                if (!db.objectStoreNames.contains('projects')) {
-                    db.createObjectStore('projects', { keyPath: 'id' });
-                }
-                if (!db.objectStoreNames.contains('files')) {
-                    const fileStore = db.createObjectStore('files', { keyPath: 'id' });
-                    fileStore.createIndex('projectId', 'projectId', { unique: false });
-                }
-            };
-            request.onsuccess = (e) => {
-                this.db = e.target.result;
-                resolve(this.db);
-            };
-            request.onerror = (e) => reject(e.target.error);
-        });
-    }
-
-    async getAllProjects() {
-        return new Promise((resolve, reject) => {
-            const tx = this.db.transaction('projects', 'readonly');
-            const store = tx.objectStore('projects');
-            const req = store.getAll();
-            req.onsuccess = () => resolve(req.result || []);
-            req.onerror = () => reject(req.error);
-        });
-    }
-
-    async createProject(project) {
-        return new Promise((resolve, reject) => {
-            const tx = this.db.transaction(['projects', 'files'], 'readwrite');
-            const projectStore = tx.objectStore('projects');
-            const fileStore = tx.objectStore('files');
-
-            projectStore.add(project);
-
-            const modFile = {
-                id: `${project.id}_mod_json`,
-                projectId: project.id,
-                name: 'mod.json',
-                path: 'mod.json',
-                type: 'json',
-                content: JSON.stringify({
-                    name: project.name,
-                    displayName: project.name,
-                    author: project.author,
-                    description: project.description,
-                    version: project.version,
-                    minGameVersion: '146'
-                }, null, 2)
-            };
-            fileStore.add(modFile);
-
-            tx.oncomplete = () => resolve(project);
-            tx.onerror = () => reject(tx.error);
-        });
-    }
-
-    async getProjectFiles(projectId) {
-        return new Promise((resolve, reject) => {
-            const tx = this.db.transaction('files', 'readonly');
-            const store = tx.objectStore('files');
-            const index = store.index('projectId');
-            const req = index.getAll(projectId);
-            req.onsuccess = () => resolve(req.result || []);
-            req.onerror = () => reject(req.error);
-        });
-    }
-
-    async saveFile(file) {
-        return new Promise((resolve, reject) => {
-            const tx = this.db.transaction('files', 'readwrite');
-            const store = tx.objectStore('files');
-            const req = store.put(file);
-            req.onsuccess = () => resolve(req.result);
-            req.onerror = () => reject(req.error);
-        });
-    }
-}
-
-const db = new Database();
+  }
+};
